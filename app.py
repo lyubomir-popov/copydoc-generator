182ebd254a52910c8ed2ab223971cdcf019777c9
Lyubomir Popov <lyubomir-popov@gmail.com>
2019-02-27 15:57:06 +0100

    add google apis; handle POST request

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from flask import Flask, render_template, request
 
 app = Flask(
     __name__,
@@ -6,10 +12,65 @@ app = Flask(
     static_folder="build/static",
 )
 
+SCOPES = ['https://www.googleapis.com/auth/documents',
+          'https://www.googleapis.com/auth/drive.appdata',
+          'https://www.googleapis.com/auth/drive']
+
+
+# The ID of a sample document.
+DOCUMENT_ID = '1FOrHyHTlRTC6MWIjdAIGvGGOfUNTJwEgWXbQ_QYT7hM'
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/upload/drive", methods=["POST"])
+# @app.route("/drive", methods=["POST"])
+@app.route("/drive", methods=["POST"])
 def upload_to_drive():
+    body = request.get_json()
+    print(body.get('amount'))
+
+    return "Ola"
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('docs', 'v1', credentials=creds)
+
+    # create doc
+
+    title = 'My Document'
+    body = {
+        'title': title
+    }
+    # doc = service.documents().create(body=body).execute()
+    doc = service.documents().get(documentId=DOCUMENT_ID).execute()
+    print('Created document with title: {0}'.format(
+        doc.get('title')))
+
+    # add text
+    requests = [
+         {
+            'insertText': {
+                'location': {
+                    'index': 1,
+                },
+                'text': 'Heading 3-5 words max no stop writing too long...'
+            }
+        }
+    ]
+
+    result = service.documents().batchUpdate(documentId=DOCUMENT_ID, body={'requests': requests}).execute()
     return "UPLOADED"
