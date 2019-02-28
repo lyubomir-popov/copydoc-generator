922d9dc44d022e9f169686d0f48e048b130f9c50
lyubomir <lyubomir.popov@gmail.com>
2019-02-28 11:24:44 +0100

    Insert text into google doc using merge (#2)
    Add simple post request to edit existing google doc

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from flask import Flask, render_template, request
+import datetime
 
 app = Flask(
     __name__,
@@ -6,10 +13,73 @@ app = Flask(
     static_folder="build/static",
 )
 
+SCOPES = ['https://www.googleapis.com/auth/documents',
+          'https://www.googleapis.com/auth/drive.appdata',
+          'https://www.googleapis.com/auth/drive']
+
+
+# The ID of a sample document.
+DOCUMENT_ID = '1FOrHyHTlRTC6MWIjdAIGvGGOfUNTJwEgWXbQ_QYT7hM'
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/upload/drive", methods=["POST"])
-def upload_to_drive():
-    return "UPLOADED"
+# @app.route("/drive", methods=["POST"])
+@app.route("/drive", methods=["POST"])
+def append_text():
+    body = request.get_json()
+
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('docs', 'v1', credentials=creds, cache_discovery=False)
+
+
+    # create doc
+
+    # title = 'My Document'
+    # body = {
+    #     'title': title
+    # }
+    # doc = service.documents().create(body=body).execute()
+    doc = service.documents().get(documentId=DOCUMENT_ID).execute()
+    # add text
+    # print(body.get('heading'))
+    heading = body.get('heading')
+    date = datetime.datetime.now().strftime("%y/%m/%d")
+    requests = [
+            {
+                'replaceAllText': {
+                    'containsText': {
+                        'text': '{{title}}',
+                        'matchCase':  'true'
+                    },
+                    'replaceText': heading,
+                }}, {
+                'replaceAllText': {
+                    'containsText': {
+                        'text': '{{date}}',
+                        'matchCase':  'true'
+                    },
+                    'replaceText': str(date),
+                }
+            }
+        ]
+
+    result = service.documents().batchUpdate(documentId=DOCUMENT_ID, body={'requests': requests}).execute()
+    print('String appended to {0}'.format(doc.get('title')))
+    return "Done"
